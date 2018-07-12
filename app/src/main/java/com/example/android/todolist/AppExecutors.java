d1b0cb1792ef9e71a7593bbbc4f3ae3f15421b99
kawnayeen <kawnayeen.08@gmail.com>
2018-07-12 15:52:23 +0600

    showling list of task at main activity, executors created

---
+++ app/src/main/java/com/example/android/todolist/AppExecutors.java
@@ -0,0 +1,55 @@
+package com.example.android.todolist;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class AppExecutors {
+    private static final Object LOCK = new Object();
+    private static AppExecutors appExecutors;
+    private final Executor diskIO;
+    private final Executor mainThread;
+    private final Executor networkIO;
+
+    private AppExecutors(Executor diskIO, Executor networkIO, Executor mainThread) {
+        this.diskIO = diskIO;
+        this.networkIO = networkIO;
+        this.mainThread = mainThread;
+    }
+
+    public static AppExecutors getInstance() {
+        if (appExecutors == null) {
+            synchronized (LOCK) {
+                appExecutors = new AppExecutors(Executors.newSingleThreadExecutor(), Executors.newFixedThreadPool(3),
+                        new MainThreadExecutor()
+                );
+            }
+        }
+        return appExecutors;
+    }
+
+    public Executor diskIO() {
+        return diskIO;
+    }
+
+    public Executor getMainThread() {
+        return mainThread;
+    }
+
+    public Executor getNetworkIO() {
+        return networkIO;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+}
